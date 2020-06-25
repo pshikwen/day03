5f2e7370719404e5e6d7d32c736a032d9f4db880
Pfumelo Shikweni <pshikwen@c4r13s8.wethinkcode.co.za>
2020-06-25 09:20:57 +0200

    done

---
+++ ex05/ft_putstr.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+int ft_putchar( char c);
+
+int		ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	if (str)
+	{
+		while (*str != '\0')
+		{
+			ft_putchar(*str);
+			++str;
+		}
+	}
+}
